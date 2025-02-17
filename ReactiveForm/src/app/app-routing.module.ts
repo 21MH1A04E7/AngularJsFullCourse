f243a7143e4b58652fed90db8ff2447a63b60b04
hariom <hariom@akriviain.onmicrosoft.com>
2025-02-17 22:53:14 +0530

    laptop changes

--- ReactiveForm/src/app/app-routing.module.ts
+++ ReactiveForm/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'form',
+    loadChildren: () =>
+      import('./forms/forms.module').then((m) => m.FormsModule),
+  },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
